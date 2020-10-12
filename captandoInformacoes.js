dfd45d003b6d0351f4fb676d7feff948563456a7
amandahp <amandahoffmann96@gmail.com>
2020-10-12 08:55:43 -0300

    Ex 1

---
+++ captandoInformacoes.js
@@ -0,0 +1,12 @@
+//Para limpar console, basta executar o comando clear()
+
+//Comando de entrada de dados
+var nome = prompt("Informe seu nome", "Digite aqui");
+var idade = prompt("Informe sua idade", "Digite aqui");
+
+/**Comando de exibição de dados
+ * Eu utilizei alert para exibir os dados em uma caixa de diálogo, 
+ * mas você pode estar optando por dar um console.log, 
+ * dessa maneira vc irá exibir sua msg no terminal.
+*/
+alert("Seja bem vindo" + " " + nome + "!" + " " + "Você tem " + idade + " " + "anos.");
